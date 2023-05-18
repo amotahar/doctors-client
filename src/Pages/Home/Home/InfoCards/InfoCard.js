74332d197a4c245bdfeeb09af591b383a23d36f8
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-05-18 18:42:28 +0600

    info card

--- src/Pages/Home/Home/InfoCards/InfoCard.js
+++ src/Pages/Home/Home/InfoCards/InfoCard.js
@@ -1,18 +1,51 @@
 import React from 'react';
 
 const InfoCard = ({card}) => {
-  const {name, description, icon, bgClass } = card; 
+  const {name, icon, description, bgClass} = card;
   return (
-    <div className={`card text-white p-6 card-side shadow-xl ${bgClass}`}>
-    <figure>
-      <img src={icon} alt="Movie"/>
+    <div className={`card px-6 text-white card-side shadow-xl ${bgClass}`}>
+   <figure>
+     <img src={icon} alt="Movie"/>
     </figure>
-    <div className="card-body">
-      <h2 className="card-title">{name}</h2>
-      <p>{description}</p>
-    </div>
+  <div className="card-body">
+    <h2 className="card-title">{name}</h2>
+    <p>{description}</p>
   </div>
+</div>
   );
 };
 
 export default InfoCard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// const InfoCard = ({card}) => {
+//   const {name, description, icon, bgClass } = card; 
+//   return (
+//     <div className={`card text-white p-6 card-side shadow-xl ${bgClass}`}>
+//     <figure>
+//       <img src={icon} alt="Movie"/>
+//     </figure>
+//     <div className="card-body">
+//       <h2 className="card-title">{name}</h2>
+//       <p>{description}</p>
+//     </div>
+//   </div>
+//   );
+// };
+
+// export default InfoCard;
--- src/Pages/Home/Home/InfoCards/InfoCards.js
+++ src/Pages/Home/Home/InfoCards/InfoCards.js
@@ -1,45 +1,102 @@
 import React from 'react';
-import clock from '../../../../assets/icons/clock.svg'
-import marker from '../../../../assets/icons/marker.svg'
-import phone from '../../../../assets/icons/phone.svg'
+import clock from '../.../../../../../assets/icons/clock.svg';
+import marker from '../.../../../../../assets/icons/marker.svg';
+import phone from '../.../../../../../assets/icons/phone.svg';
 import InfoCard from './InfoCard';
 
 const InfoCards = () => {
-
-  const cardData = [
-    {
+  const cardData = [ 
+    { 
       id: 1,
-      name: 'Opening Hours',
-      description: 'Open 9 am to 10 pm',
       icon: clock,
-      bgClass: 'bg-gradient-to-r from-primary to-secondary',
+      name: 'Opening Hour',
+      description: "Open 10 am to 10 pm",
+      bgClass: 'bg-gradient-to-r from-primary to-secondary'
     },
-    {
+    { 
       id: 2,
-      name: 'Visit Our Loacation',
-      description: 'Dhaka, Bangladesh',
       icon: marker,
-      bgClass: 'bg-accent',
+      name: 'Visit Our Location',
+      description: "Dhaka, Bangladesh",
+      bgClass: 'bg-accent'
     },
-    {
+    { 
       id: 3,
-      name: 'Contact us now',
-      description: '14678656789',
       icon: phone,
-      bgClass: 'bg-gradient-to-r from-primary to-secondary',
+      name: 'Contact Us',
+      description: "01287615313",
+      bgClass: 'bg-gradient-to-r from-primary to-secondary'
     },
-
   ]
-    return (
-    <div className='grid mt-8 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-      {
-        cardData.map(card=><InfoCard
-           key={card.id}
-           card={card}
-        ></InfoCard>)
-      }
+  return (
+    <div className='grid mt-8 gap-6 grid-cols-1 md:grid-cols2 lg:grid-cols-3'>
+        {
+          cardData.map(card=> <InfoCard
+               key={card.id}
+               card={card}
+          ></InfoCard>)
+        }
     </div>
   );
 };
 
 export default InfoCards;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import clock from '../../../../assets/icons/clock.svg'
+// import marker from '../../../../assets/icons/marker.svg'
+// import phone from '../../../../assets/icons/phone.svg'
+// import InfoCard from './InfoCard';
+
+// const InfoCards = () => {
+
+//   const cardData = [
+//     {
+//       id: 1,
+//       name: 'Opening Hours',
+//       description: 'Open 9 am to 10 pm',
+//       icon: clock,
+//       bgClass: 'bg-gradient-to-r from-primary to-secondary',
+//     },
+//     {
+//       id: 2,
+//       name: 'Visit Our Loacation',
+//       description: 'Dhaka, Bangladesh',
+//       icon: marker,
+//       bgClass: 'bg-accent',
+//     },
+//     {
+//       id: 3,
+//       name: 'Contact us now',
+//       description: '14678656789',
+//       icon: phone,
+//       bgClass: 'bg-gradient-to-r from-primary to-secondary',
+//     },
+
+//   ]
+//     return (
+//     <div className='grid mt-8 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+//       {
+//         cardData.map(card=><InfoCard
+//            key={card.id}
+//            card={card}
+//         ></InfoCard>)
+//       }
+//     </div>
+//   );
+// };
+
+// export default InfoCards;
