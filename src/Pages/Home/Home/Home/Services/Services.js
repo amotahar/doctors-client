e3f5a425c55997d31e898687b45f8dbad009b9e8
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-05-19 20:19:53 +0600

    treatment2

--- src/Pages/Home/Home/Home/Services/Services.js
+++ src/Pages/Home/Home/Home/Services/Services.js
@@ -45,16 +45,17 @@ const Services = () => {
         </div>
       </div>
       <div className="mt-6 grid">
-        <div className="card grid-flex bg-base-100">
-           <div>
-           <figure className="w-1/2">
-            <img src={treatment} alt="Movie" />
-          </figure>
-           </div>
-          <div className="card-body">
-            <h2 className="card-title">Exceptional Dental Care, on Your Terms</h2>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page </p>
-            <div className="card-actions justify-start">
+        <div className="hero bg-white">
+          <div className="hero-content flex-col lg:flex-row">
+            <img alt=""
+              src={treatment}
+              className="max-w-sm rounded-lg lg:w-1/2 shadow-2xl"
+            />
+            <div className="mx-14">
+              <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+              <p className="py-6">
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page 
+              </p>
               <button className="btn btn-primary text-white bg-gradient-to-r from-primary to-secondary">
                 Get Started
               </button>
