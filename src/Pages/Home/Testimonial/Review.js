ec5eab459af198d37cd61341fc60335cc85f91d9
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-05-20 10:21:26 +0600

    footer

--- src/Pages/Home/Testimonial/Review.js
+++ src/Pages/Home/Testimonial/Review.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-const Review = ({ Review }) => {
-  const { name, img, review, location } = review;
+const Review = ({ review }) => {
+  const { name, img, review: userReview, location } = review;
   return (
     <div className="card shadow-xl">
       <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <p>{userReview}</p>
+        <div className="flex items-center mt-6">
+          <div className="avatar mr-6">
+            <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <h5 className="text-lg">{name}</h5>
+              <p>{location}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
