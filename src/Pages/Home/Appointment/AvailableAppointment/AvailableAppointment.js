0521009515732bc6a820bf3032027be724b63691
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-05-21 13:33:34 +0600

    appointment

--- src/Pages/Home/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Home/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,11 +1,30 @@
-import { format } from 'date-fns';
-import React from 'react';
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import AppointmentOption from "./AppointmentOption";
 
-const AvailableAppointment = ({selectedDate}) => {
+const AvailableAppointment = ({ selectedDate }) => {
+  const [appointmentOptions, setAppointmentOptions] = useState([]);
+
+  useEffect(() => {
+    fetch("AppointmentOptions.json")
+      .then((res) => res.json())
+      .then((data) => setAppointmentOptions(data));
+  }, []);
   return (
-    <section className='mt-16'>
-       <p className='text-center text-secondary font-bold'>Available Services on  {format(selectedDate, "PP")}</p>
-       <p>Please select a service.</p>
+    <section className="my-16">
+      <p className="text-center text-secondary font-bold">
+        Available Services on{format(selectedDate, "PP")}
+      </p>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16">
+        {
+          appointmentOptions.map(option=><AppointmentOption
+          key={option._id}
+          appointmentOption={option}
+          ></AppointmentOption>)
+        }
+
+
+      </div>
     </section>
   );
 };
